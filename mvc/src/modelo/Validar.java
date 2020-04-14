62a5c3af0981cecb9f788191dee5f0b1fbe55cfd
Yefer <Yefer@192.168.0.14>
2020-04-14 09:51:31 -0500

    Se comenta la clase Validar y su respectivo método.

--- mvc/src/modelo/Validar.java
+++ mvc/src/modelo/Validar.java
@@ -14,14 +14,20 @@ import javax.swing.JTextField;
  * @author Yefer
  */
 public class Validar {
+    /*
+    Se crea el método validar letras que consiste en capturar las teclas según se 
+    pulsan y decidir si se aceptan o no para escribir en el JTextField
+    */
     public void validarLetras(JTextField campo){
         campo.addKeyListener(new KeyAdapter(){
+            //Cada vez que se pulse una tecla, se avisará al método keyTyped(KeyEvent e) de nuestro KeyListener.
             public void keyTyped(KeyEvent e){
+                //Del KeyEvent que nos pasan como parámetro podemos obtener qué tecla se ha pulsado (qué caracter) y rechazarla o no según convenga
                 char c = e.getKeyChar();
                 
                 if(Character.isDigit(c)){
                     e.consume();
-                    System.out.println("");
+                    System.out.println("Ingrese solo letras");
                 }
             }
         });
